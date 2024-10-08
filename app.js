e034def2468d2615d48f471574dc9b07387326c5
Rehan-Paudel <rehanpoudel2@gmail.com>
2024-10-08 11:52:02 +0545

    learning to connect with databse

--- app.js
+++ app.js
@@ -1,49 +1,20 @@
-const express = require("express");
+const express =require('express');
 const app = express();
-const cors = require("cors");
-const fs = require("node:fs/promises");
-// const os = require("node:os");
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hello dai");
+const connect_database= require('./Database/connect.js');
+const model = require("./Database/user.js");
+connect_database();
+app.use(express.json());
+app.get('/', (req,res)=>{
+    res.send("Hello world !")
 });
-
-
-async function  getFiles() {
-  // const createfile = await fs.writeFile("text.txt","Hello dipson dai"); // create an new file with the given data
-// const readFile = await fs.readFile("text.txt","utf-8"); // read the data of the  file
-// const unlined = await fs.unlink("text.txt"); delete the file from the location 
-// const append = fs.appendFile("text.txt"," from rehan Paudel"); // Append some content to the already present file like updated
-// const copy = fs.copyFile("text.txt","rehan.txt") // copy the file and paste that into new file 
-// const newfolder = await fs.mkdir("Projects"); // creates an new directory 
-// const newfile = await fs.writeFile("Projects/rehan.txt","Hello rehan paudel from dispon daii ") making files inside a new dir / folder
-// const deletfolder = await fs.rmdir("Projects") // remove the folder  and all the files inside it
-// const file = await fs.readdir("Projects"); // it will read the files or return the files inside a particular directory orfolder 
-// console.log(file.length);
-
-// let detils = await fs.stat("text.txt"); // give proper details abou the file like size , creat time , append time and modified time
-
-// console.log(detils);
-
-
-
-}
-
-
-getFiles()
-
-
-
-
-
-app.listen(7000, () => {
-  console.log("Server is running on port 3000");
+app.post("/data",async(req,res)=>{
+const {name,pass}  = req.body;
+console.log(name,pass);
+await model.create({
+    name : name,
+    password : pass,
+})
+})
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
 });
-
-
-// WriteFile creates a new file
-// readFile read the data of the file
-// Unlink delete  the file
-
